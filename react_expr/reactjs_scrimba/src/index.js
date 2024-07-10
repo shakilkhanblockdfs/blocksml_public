655a7c586022227be6cb4febba5b2192f18bdad0
shakil <shakil.khan@blockdfs.com>
2024-07-10 00:16:02 -0700

    Added the page with Nav, header, footer with the react component for a single page

--- react_expr/reactjs_scrimba/src/index.js
+++ react_expr/reactjs_scrimba/src/index.js
@@ -1,22 +1,40 @@
-import React from "react";
-// import ReactDOM from "react-dom/client";
-import ReactDOM from "react-dom";
+/**
+Challenge: 
 
-function MyApplication(){
-    return (
-        <div>
-            <img src="react-logo.png" alt="React Logo" />
-            <h1> Fun Facts about React</h1>
-            <ul>
-                <li>Was first release in 2013</li>
-                <li>Was Originally created by Jordan Walke</li>
-                <li>Was Created by Facebook</li>
-            </ul>
-        </div>
-    )
-}
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
-
-
-// ReactDOM.createRoot(document.getElementById("root")).render(page);
-ReactDOM.render(<MyApplication />,document.getElementById("root"))
+    import React from "react"
+    import ReactDOM from "react-dom"
+    
+    function Page() {
+        return (
+            <div>
+                <header>
+                    <nav>
+                        <img src="./react-logo.png" width="40px" />
+                    </nav>
+                </header>
+                <h1>Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>It's a popular library, so I'll be 
+                    able to fit in with the cool kids!</li>
+                    <li>I'm more likely to get a job as a developer
+                    if I know React</li>
+                </ol>
+                <footer>
+                    <small>© 2021 Khan development. All rights reserved.</small>
+                </footer>
+            </div>
+        )
+    }
+    
+    ReactDOM.render(<Page />, document.getElementById("root"))
