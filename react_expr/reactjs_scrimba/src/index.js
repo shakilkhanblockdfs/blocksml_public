038510fd57f3b2b847d292199cf9543560370855
shakil <shakil.khan@blockdfs.com>
2024-07-09 18:48:20 -0700

    JSX displayed by root.append, will have either object object or dom elements but not displayed correctly

--- react_expr/reactjs_scrimba/src/index.js
+++ react_expr/reactjs_scrimba/src/index.js
@@ -12,11 +12,21 @@ const navbar = (
             <li>About</li>
             <li>Contact1234</li>
         </ul>
+        <p> This is a paragrpah tag123</p>
     </nav>
 );
 
 // ReactDOM.render(navbar, document.getElementById("root"));
 // ReactDOM.createRoot(document.getElementById("root")).render(navbar);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(navbar);
+
+// Process 1, but this will not display jsx properly rather it will just dump it
+// let root = document.getElementById("root");
+// console.log(root);
+// root.append(JSON.stringify(navbar));
+
+// Process:2 Correct way of displaying the JSX 
+ReactDOM.createRoot(document.getElementById("root")).render(navbar);
+
