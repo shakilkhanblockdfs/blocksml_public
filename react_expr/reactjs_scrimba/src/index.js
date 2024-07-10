e08cdc34ef6177b19f4a3dc321d46eaa6c6ae7fd
shakil <shakil.khan@blockdfs.com>
2024-07-09 20:04:33 -0700

    React as a component

--- react_expr/reactjs_scrimba/src/index.js
+++ react_expr/reactjs_scrimba/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const page = (
+function MyApplication(){
+    return (
         <div>
             <img src="react-logo.png" alt="React Logo" />
             <h1> Fun Facts about React</h1>
@@ -11,8 +13,10 @@ const page = (
                 <li>Was Created by Facebook</li>
             </ul>
         </div>
-);
+    )
+}
 
-// console.log(page)
 
-ReactDOM.createRoot(document.getElementById("root")).render(page);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(page);
+ReactDOM.render(<MyApplication />,document.getElementById("root"))
