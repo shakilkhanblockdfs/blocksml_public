daa0c5aeaa9e129b96d472bf66990fd25fdb1c29
shakil <shakil.khan@blockdfs.com>
2024-07-10 10:39:00 -0700

    Separated the Header, Footer and BodyComponent into separate files

--- react_expr/reactjs_scrimba/src/index.js
+++ react_expr/reactjs_scrimba/src/index.js
@@ -14,46 +14,9 @@ Part 2:
 
     import React from "react"
     import ReactDOM from "react-dom"
-    
-    function HeaderComponent() {
-        return (    
-            <header>
-                <nav className="nav">
-                    
-                    <img className="img-style" src="./react-logo.png" width="40px" />
-                    <ul className="nav-item">
-                        <li>Pricing</li>    
-                        <li>About</li>    
-                        <li>Contact</li>
-                    </ul>   
-                    <h3> ReactJS Separate HeaderComponent </h3>
-
-                </nav>
-            </header>
-        )
-    }
-
-    function FooterComponent() {
-        return (
-            <footer>
-                <small>© 2021 Khan development. All rights reserved.</small>
-            </footer>
-        )
-    }   
-
-function BodyComponent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be
-                    able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                    if I know React</li>
-            </ol>
-        </div>
-    )
-}   
+    import HeaderComponent from "./Header"
+    import BodyComponent from "./BodyComponent"
+    import FooterComponent from "./Footer"  
 
     function Page() {
         return (
