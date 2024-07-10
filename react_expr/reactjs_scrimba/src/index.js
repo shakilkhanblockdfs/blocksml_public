51a0b1c518ff7e736353e2f579a408fc16685b65
shakil <shakil.khan@blockdfs.com>
2024-07-10 00:45:22 -0700

    Created a seorate component for Header, Footer and Main body component

--- react_expr/reactjs_scrimba/src/index.js
+++ react_expr/reactjs_scrimba/src/index.js
@@ -15,24 +15,47 @@ Part 2:
     import React from "react"
     import ReactDOM from "react-dom"
     
+    function HeaderComponent() {
+        return (    
+            <header>
+                <nav>
+                    
+                    <img src="./react-logo.png" width="40px" />
+                    <h3> ReactJS Separate HeaderComponent </h3>
+
+                </nav>
+            </header>
+        )
+    }
+
+    function FooterComponent() {
+        return (
+            <footer>
+                <small>© 2021 Khan development. All rights reserved.</small>
+            </footer>
+        )
+    }   
+
+function BodyComponent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be
+                    able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                    if I know React</li>
+            </ol>
+        </div>
+    )
+}   
+
     function Page() {
         return (
             <div>
-                <header>
-                    <nav>
-                        <img src="./react-logo.png" width="40px" />
-                    </nav>
-                </header>
-                <h1>Reasons I'm excited to learn React</h1>
-                <ol>
-                    <li>It's a popular library, so I'll be 
-                    able to fit in with the cool kids!</li>
-                    <li>I'm more likely to get a job as a developer
-                    if I know React</li>
-                </ol>
-                <footer>
-                    <small>© 2021 Khan development. All rights reserved.</small>
-                </footer>
+                <HeaderComponent />    
+                <BodyComponent />
+                <FooterComponent />
             </div>
         )
     }
