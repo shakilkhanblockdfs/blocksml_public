5f4cd9d694726bb74c9836e4ff5ae1e1f98a9d38
shakil <shakilk1729@gmail.com>
2024-10-18 06:07:45 -0700

    Dev changes

--- react_expr/todo-app-with-sqlite3/fronend_ui/vite.config.js
+++ react_expr/todo-app-with-sqlite3/fronend_ui/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+		host: '0.0.0.0', // Bind to all IP addresses
     port: 8000,  // Specify the port you want to use
   },
 })
